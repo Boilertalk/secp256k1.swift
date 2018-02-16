6815a8eac1ead2a4abaad055570c3c61198b6b11
Koray Koska <koray@koska.at>
2018-02-16 21:33:14 +0100

    Try to fix Package.swift?

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "secp256k1",
-            targets: ["secp256k1"]),
+            targets: ["libsecp256k1"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,7 +18,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "secp256k1",
+            name: "libsecp256k1",
             path: "./secp256k1/Classes",
             exclude: [
                 "secp256k1/build-aux",
