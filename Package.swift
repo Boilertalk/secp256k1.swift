3a985d6a5a72f85847e89c4d7c455fe187eeb662
Koray Koska <koray@koska.at>
2018-02-16 03:55:53 +0100

    Add test files to Package.swift excludes

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         .target(
             name: "secp256k1",
             path: "./secp256k1/Classes",
-            exclude: ["secp256k1/build-aux", "secp256k1/obj", "secp256k1/sage", "secp256k1/src/asm", "secp256k1/src/java"],
+            exclude: ["secp256k1/build-aux", "secp256k1/obj", "secp256k1/sage", "secp256k1/src/asm", "secp256k1/src/java", "secp256k1/src/testrand_impl.h", "secp256k1/src/testrand.h", "secp256k1/src/tests_exhaustive.c", "secp256k1/src/tests.c", "secp256k1/src/gen_context.c", "secp256k1/src/*bench*.{c,h}", "src/modules/{recovery,ecdh}/*test*.{c,h}"],
             sources: [".", "secp256k1/src", "secp256k1/include", "secp256k1/contrib", "secp256k1/modules/ecdh", "secp256k1/modules/recovery"],
             publicHeadersPath: "secp256k1/include"),
     ]
