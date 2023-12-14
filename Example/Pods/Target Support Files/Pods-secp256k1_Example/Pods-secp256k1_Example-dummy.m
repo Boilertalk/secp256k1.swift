3c0b628b2f1d6dee9e8ff2cce1e74f1ba577e8ce
Nhat Dinh <nhatphutho@gmail.com>
2023-12-14 13:09:01 +0900

    remove and config

--- Example/Pods/Target Support Files/Pods-secp256k1_Example/Pods-secp256k1_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_secp256k1_Example : NSObject
-@end
-@implementation PodsDummy_Pods_secp256k1_Example
-@end
--- Example/Tests/Tests.swift
+++
@@ -1,9 +0,0 @@
-
-import XCTest
-
-class Test: XCTest {
-	
-	func testExample() {
-		XCTAssert(10 == 10)
-	}
-}
