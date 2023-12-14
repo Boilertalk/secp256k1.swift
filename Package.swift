a57ebab39e1e8956a9fc20ff10c9303d2e588aad
Nhat Dinh <nhatphutho@gmail.com>
2023-12-14 14:17:38 +0900

    more config

--- Package.swift
+++ Package.swift
@@ -37,10 +37,21 @@ let package = Package(
                 "secp256k1/src/bench_recover.c",
                 "secp256k1/src/bench_sign.c",
                 "secp256k1/src/bench_verify.c",
+                "secp256k1/src/bench_generator.c",
+                "secp256k1/src/bench_rangeproof.c",
+                "secp256k1/src/bench_bppp.c",
+                "secp256k1/src/bench_whitelist.c",
+
+                "secp256k1/src/precompute_ecmult.c",
+                "secp256k1/src/precompute_ecmult_gen.c",
+                "secp256k1/src/valgrind_ctime_test.c",
+                
                 "secp256k1/src/bench.h",
                 "secp256k1/src/modules/ecdh/tests_impl.h",
                 "secp256k1/src/modules/recovery/tests_impl.h",
+                "secp256k1/src/precomputed_ecmult_gen.c",
 
+                "secp256k1/examples",
                 "exporter"
             ],
             sources: [
@@ -53,8 +64,23 @@ let package = Package(
             ],
             publicHeadersPath: "secp256k1/include",
             cSettings: [
-                .headerSearchPath("secp256k1/src")
-            ]
+               .define("ENABLE_MODULE_ECDH"),
+               .define("ENABLE_MODULE_EXTRAKEYS"),
+               .define("ENABLE_MODULE_RECOVERY"),
+               .define("ENABLE_MODULE_SCHNORRSIG"),
+               .define("ECMULT_GEN_PREC_BITS=4"),
+               .define("ECMULT_WINDOW_SIZE=15"),
+               .define("ENABLE_MODULE_ECDSA_ADAPTOR"),
+               .define("ENABLE_MODULE_ECDSA_S2C"),
+               .define("ENABLE_MODULE_GENERATOR"),
+               .define("ENABLE_MODULE_RANGEPROOF"),
+               .define("ENABLE_MODULE_SURJECTIONPROOF"),
+               .define("ENABLE_MODULE_WHITELIST"),
+               .define("ENABLE_MODULE_MUSIG"),
+               .define("ENABLE_MODULE_EXPERIMENTAL"),
+
+               .headerSearchPath("secp256k1/src")
+           ]
         ),
         .target(
             name: "secp256k1",
